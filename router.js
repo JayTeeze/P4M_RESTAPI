5d9e79535ac492eac4c3b6a51f965fdeaa5efaf5
Adrian Ortiz <ortizalvaradoja@gmail.com>
2022-12-24 00:28:05 -0500

    Commit/push unstaged file.

--- router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const choices = require('./controller/choices');
-
-router.route('/choices')
-    .get(choices.get);
-
-module.exports = router;
