bb390b2cb266c6b8aaa790117eb346877d23123b
Adrian Ortiz <ortizalvaradoja@gmail.com>
2022-12-24 00:24:50 -0500

    Implemented shutdown logic to handle graceful shutdown.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ async function startup() {
     console.log('Starting application');
     
     try {
+        console.log('Starting web server');
         await webServer.start();
     } catch(err) {
         console.log('Encountered error', err);
@@ -12,6 +13,25 @@ async function startup() {
 }
 
 startup();
+
+async function shutdown() {
+    try {
+        await webServer.close();
+        process.exit(0);
+    } catch(err) {
+        process.exit(1);
+    }
+}
+
+process.on('SIGINT', () => {
+    console.log('SIGINT received');
+    shutdown();
+})
+
+process.on('SIGTERM', () => {
+    console.log('SIGTERM received');
+    shutdown();
+})
 
 // const express = require('express');
 // const app = express();
--- service/web-server.js
+++ service/web-server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const router = require('../router');
+const router = require('./router');
 
 let app, httpServer;
 
@@ -20,21 +20,42 @@ function start() {
 
         app.use(cors());
     
-        // TODO make router module
         app.use('/api', router);
     
-        httpServer.listen(port, (err) => {
-            if (err) {
-                reject(err);
-                return;
-            }
+        // httpServer.listen(port, (err) => {
+        //     if (err) {
+        //         reject(err);
+        //         return;
+        //     }
 
+        //     console.log(`Web server listening on port: ${port}`);
+
+        //     resolve();
+        // });
+
+        httpServer.listen(port).on('listening', () => {
             console.log(`Web server listening on port: ${port}`);
-
             resolve();
-        });
+        }).on('error', (err) => {
+            reject(err);
+        })
     })
 
 }
 
 module.exports.start = start;
+
+function close() {
+    return new Promise((resolve, reject) => {
+        console.log('Closing web server');
+        httpServer.close().on('error', (err) => {
+            reject(err);
+            return;
+        });
+        console.log('Web server closed');
+        resolve();
+
+    })
+}
+
+module.exports.close = close;
