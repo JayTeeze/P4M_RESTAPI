d417109411a698fdb879f35ecb80bb2461192753
Adrian Ortiz <ortizalvaradoja@gmail.com>
2023-01-01 22:28:33 -0500

    Refactored logic to implement sqlite DB with error handling and input validation.

--- controller/picker.js
+++ controller/picker.js
@@ -1,8 +1,18 @@
 const picker = require('../db_apis/picker');
 
-function get(req, res, next) {
-    let choice = picker.select();
-    res.status(200).json(choice);
+async function get(req, res, next) {
+    try {
+        const result = await picker.select();
+
+        if (result.length > 0) {
+            res.status(200).json(result[0]);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        console.log(err);
+        res.sendStatus(500);
+    }
 }
 
 module.exports.get = get;
--- db_apis/picker.js
+++ db_apis/picker.js
@@ -1,7 +1,11 @@
-const choices = require('../db_apis/choices').data;
+const database = require('../service/database');
 
-function select() {
-    return choices[Math.floor(Math.random() * choices.length)];
+async function select() {
+    let query = `SELECT * FROM choice ORDER BY RANDOM() LIMIT 1`;
+    let binds = [];
+
+    const result = await database.simpleExecute(query, binds, false);
+    return result;
 }
 
 module.exports.select = select;
